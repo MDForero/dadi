5ca3cfe0c5d020b75a229078747bc666724a6bf0
Michael Forero <mforeroroldan@gmail.com>
2023-02-11 15:48:25 -0500

    poster

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 import video from "../assets/video-drone.mp4"
+import drone from "../assets/silueta-hombre-pilotando-avion-no-tripulado-al-atardecer-cielo-soleado-segundo-plano.jpg"
 
 const Video = () => {
     return (
-        <video src={video} autoPlay muted loop></video>
+        <video src={video} autoPlay muted loop poster={drone}></video>
     )
 }
 
